824461637d2f7d356eb29b46a4fef8bb22d3a0e5
ExtraTea <d-takamatsu@g.ecc.u-tokyo.ac.jp>
2023-11-16 15:41:47 +0900

    sync

---
+++ crowd_nav/train copy.py
@@ -0,0 +1,95 @@
+import sys
+import logging
+import argparse
+import configparser
+import os
+import shutil
+import torch
+import gymnasium as gym
+import git
+from crowd_sim.envs.utils.robot import Robot
+from crowd_nav.utils.trainer import Trainer
+from crowd_nav.utils.memory import ReplayMemory
+from crowd_nav.utils.explorer import Explorer
+from crowd_nav.policy.policy_factory import policy_factory
+
+from stable_baselines3 import PPO
+def main():
+    parser = argparse.ArgumentParser('Parse configuration file')
+    parser.add_argument('--env_config', type=str, default='configs/env.config')
+    parser.add_argument('--policy', type=str, default='cadrl')
+    parser.add_argument('--policy_config', type=str, default='configs/policy.config')
+    parser.add_argument('--train_config', type=str, default='configs/train.config')
+    parser.add_argument('--output_dir', type=str, default='data/output')
+    parser.add_argument('--weights', type=str)
+    parser.add_argument('--resume', default=False, action='store_true')
+    parser.add_argument('--gpu', default=False, action='store_true')
+    parser.add_argument('--debug', default=False, action='store_true')
+    args = parser.parse_args()
+
+    # configure paths
+    make_new_dir = True
+    if os.path.exists(args.output_dir):
+        key = input('Output directory already exists! Overwrite the folder? (y/n)')
+        if key == 'y' and not args.resume:
+            shutil.rmtree(args.output_dir)
+        else:
+            make_new_dir = False
+            args.env_config = os.path.join(args.output_dir, os.path.basename(args.env_config))
+            args.policy_config = os.path.join(args.output_dir, os.path.basename(args.policy_config))
+            args.train_config = os.path.join(args.output_dir, os.path.basename(args.train_config))
+    if make_new_dir:
+        os.makedirs(args.output_dir)
+        shutil.copy(args.env_config, args.output_dir)
+        shutil.copy(args.policy_config, args.output_dir)
+        shutil.copy(args.train_config, args.output_dir)
+    log_file = os.path.join(args.output_dir, 'output.log')
+    il_weight_file = os.path.join(args.output_dir, 'il_model.pth')
+    rl_weight_file = os.path.join(args.output_dir, 'rl_model.pth')
+
+    # configure logging
+    mode = 'a' if args.resume else 'w'
+    file_handler = logging.FileHandler(log_file, mode=mode)
+    stdout_handler = logging.StreamHandler(sys.stdout)
+    level = logging.INFO if not args.debug else logging.DEBUG
+    logging.basicConfig(level=level, handlers=[stdout_handler, file_handler],
+                        format='%(asctime)s, %(levelname)s: %(message)s', datefmt="%Y-%m-%d %H:%M:%S")
+    repo = git.Repo(search_parent_directories=True)
+    logging.info('Current git head hash code: %s'.format(repo.head.object.hexsha))
+    device = torch.device("cuda:0" if torch.cuda.is_available() and args.gpu else "cpu")
+    logging.info('Using device: %s', device)
+
+    # configure policy
+    policy = policy_factory[args.policy]()
+    if not policy.trainable:
+        parser.error('Policy has to be trainable')
+    if args.policy_config is None:
+        parser.error('Policy config has to be specified for a trainable network')
+    policy_config = configparser.RawConfigParser()
+    policy_config.read(args.policy_config)
+    policy.configure(policy_config)
+    policy.set_device(device)
+
+    # configure environment
+    env_config = configparser.RawConfigParser()
+    env_config.read(args.env_config)
+    env = gym.make('CrowdSim-v0')
+    env.configure(env_config)
+    robot = Robot(env_config, 'robot')
+    env.set_robot(robot)
+
+    model = PPO("MlpPolicy", env, verbose=1)
+    model.learn(total_timesteps=10000)
+
+    vec_env = model.get_env()
+    obs = vec_env.reset()
+
+    for i in range(1000):
+        action, _states = model.predict(obs)
+        obs, rewards, dones, info = vec_env.step(action)
+        vec_env.render()
+    
+    env.close()
+
+if __name__ == '__main__':
+    main()
